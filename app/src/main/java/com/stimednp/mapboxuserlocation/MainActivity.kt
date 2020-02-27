fbc025b8e3ee0dfec93a02dcb80189002af30c8a
im-o <frizgt@gmail.com>
2020-02-27 10:09:28 +0800

    :zap: Done show mapview, next my location

--- app/src/main/java/com/stimednp/mapboxuserlocation/MainActivity.kt
+++ app/src/main/java/com/stimednp/mapboxuserlocation/MainActivity.kt
@@ -1,12 +1,62 @@
 package com.stimednp.mapboxuserlocation
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
-class MainActivity : AppCompatActivity() {
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import com.mapbox.mapboxsdk.maps.Style
+import kotlinx.android.synthetic.main.activity_main.*
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mapMapbox: MapboxMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Mapbox.getInstance(this, BuildConfig.MAPBOX_ACCESS_TOKEN)
         setContentView(R.layout.activity_main)
+        map_view.onCreate(savedInstanceState)
+        map_view.getMapAsync(this)
+    }
+
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        mapMapbox = mapboxMap
+        mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        map_view.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        map_view.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        map_view.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        map_view.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        map_view.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        map_view.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        map_view.onSaveInstanceState(outState)
     }
 }
