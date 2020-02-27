6384a96e9022797e5049fc89c523f37dc753f48d
im-o <frizgt@gmail.com>
2020-02-27 12:05:05 +0800

    :zap: Yesyy finish, Tracking current my locaion/mydevice

--- app/src/main/java/com/stimednp/mapboxuserlocation/MainActivity.kt
+++ app/src/main/java/com/stimednp/mapboxuserlocation/MainActivity.kt
@@ -1,15 +1,32 @@
 package com.stimednp.mapboxuserlocation
 
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.android.core.location.*
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
+import com.stimednp.mapboxuserlocation.CompanionString.Companion.DEFAULT_INTERVAL_IN_MILLISECONDS
+import com.stimednp.mapboxuserlocation.CompanionString.Companion.DEFAULT_MAX_WAIT_TIME
 import kotlinx.android.synthetic.main.activity_main.*
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
-    private lateinit var mapMapbox: MapboxMap
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListener {
+    private lateinit var map: MapboxMap
+    private lateinit var permissionsManager: PermissionsManager
+    private lateinit var locationEngine: LocationEngine
+    private lateinit var callback: LocationChangeListeningCallback
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +37,87 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     override fun onMapReady(mapboxMap: MapboxMap) {
-        mapMapbox = mapboxMap
+        map = mapboxMap
+        callback = LocationChangeListeningCallback()
         mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+            enableLocationComponent(it)
+        }
+    }
+
+    private fun enableLocationComponent(loadedMapStyle: Style) {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            val locationComponentActivationOptions = LocationComponentActivationOptions.builder(this, loadedMapStyle)
+                .useDefaultLocationEngine(false)
+                .build()
+            map.locationComponent.apply {
+                activateLocationComponent(locationComponentActivationOptions)
+                isLocationComponentEnabled = true                       // Enable to make component visible
+                cameraMode = CameraMode.TRACKING                        // Set the component's camera mode
+                renderMode = RenderMode.COMPASS                         // Set the component's render mode
+            }
+            initLocationEngine()
+        } else {
+            permissionsManager = PermissionsManager(this)
+            permissionsManager.requestLocationPermissions(this)
+        }
+    }
+
+    private fun initLocationEngine() {
+        locationEngine = LocationEngineProvider.getBestLocationEngine(this)
+        val request = LocationEngineRequest
+            .Builder(DEFAULT_INTERVAL_IN_MILLISECONDS)
+            .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
+            .setMaxWaitTime(DEFAULT_MAX_WAIT_TIME)
+            .build()
+        locationEngine.requestLocationUpdates(request, callback, mainLooper)
+        locationEngine.getLastLocation(callback)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private inner class LocationChangeListeningCallback : LocationEngineCallback<LocationEngineResult> {
+
+        override fun onSuccess(result: LocationEngineResult?) {
+            result?.lastLocation ?: return //BECAREFULL HERE, IF NAME LOCATION UPDATE DONT USER -> val resLoc = result.lastLocation ?: return
+            if (result.lastLocation != null){
+                val lat = result.lastLocation?.latitude!!
+                val lng = result.lastLocation?.longitude!!
+                val latLng = LatLng(lat, lng)
+
+                if (result.lastLocation != null) {
+                    map.locationComponent.forceLocationUpdate(result.lastLocation)
+                    val position = CameraPosition.Builder()
+                        .target(latLng)
+                        .zoom(13.0) //disable this for not follow zoom
+                        .tilt(10.0)
+                        .build()
+                    map.animateCamera(CameraUpdateFactory.newCameraPosition(position))
+                    Toast.makeText(this@MainActivity, "Location update : $latLng", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
+
+        override fun onFailure(exception: Exception) {}
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        Toast.makeText(this, "Permission not granted!!", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            map.getStyle {
+                enableLocationComponent(it)
+            }
+        } else {
+            Toast.makeText(this, "Permission not granted!! app will be EXIT", Toast.LENGTH_LONG).show()
+            Handler().postDelayed({
+                finish()
+            }, 3000)
         }
     }
 
@@ -52,6 +148,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onDestroy() {
         super.onDestroy()
+        // Prevent leaks
+        locationEngine.removeLocationUpdates(callback)
         map_view.onDestroy()
     }
 
---
+++ app/src/main/java/com/stimednp/mapboxuserlocation/UtilsConst.kt
@@ -0,0 +1,13 @@
+package com.stimednp.mapboxuserlocation
+
+/**
+ * Created by rivaldy on Feb/27/2020.
+ * Find me on my lol Github :D -> https://github.com/im-o
+ */
+
+class CompanionString{
+    companion object {
+        const val DEFAULT_INTERVAL_IN_MILLISECONDS = 1000L
+        const val DEFAULT_MAX_WAIT_TIME = DEFAULT_INTERVAL_IN_MILLISECONDS * 5
+    }
+}
